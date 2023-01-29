23af32bea293c947a08b7bf5722766aca31c55fd
dev-faizan1 <ch.faizan1947@gmail.com>
2023-01-29 16:58:18 +0500

    initial

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -9,18 +7,8 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: ListView.builder( //Creates A lISTVIEW by specifing number of items
-        itemCount: 12, //number of items
-        itemBuilder: (context, index) { //builds all items 
-          return ElevatedButton(  //retruns a widget to paint
-            child: Text('Button $index'),
-            onPressed: () {
-              log('Button $index pressed');
-            },
-          );
-        },
-      ),
+    return const Material(
+      child: Center(child: Text('Widget Book')),
     );
   }
 }
