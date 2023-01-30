d7f02422fc0de8a011662ad21d44d613c7dad02d
dev-faizan1 <ch.faizan1947@gmail.com>
2023-01-30 18:34:13 +0500

    ....

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-
 class MyHomePage extends StatelessWidget {
-  static const home = 'listview';
 
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(child: Text('Widget Book')),
+    return const SafeArea(
+      child:  Scaffold(
+        body: Center(
+          child: Text('Widget Book'),
+        ),
+      ),
     );
   }
 }
