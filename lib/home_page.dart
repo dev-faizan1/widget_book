b07b36ed20664b47c058f05060ce6c4c69f79985
dev-faizan1 <ch.faizan1947@gmail.com>
2023-01-30 09:49:48 +0500

    iniital

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,8 +7,48 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(child: Text('Widget Book')),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          //A widget that displays its children in a vertical array.
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+
+              ///Align widget to top right
+              child: Container(
+                width: 100,
+                height: 50,
+                color: Colors.cyan,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.amber,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.purple,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 100,
+                height: 50,
+                color: Colors.orangeAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
