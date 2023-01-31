d8c8537c2c85ab0d3e2aae3fa81476ecd9a32fa8
dev-faizan1 <ch.faizan1947@gmail.com>
2023-01-31 18:45:14 +0500

    initial

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-class MyHomePage extends StatelessWidget {
 
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child:  Scaffold(
-        body: Center(
-          child: Text('Widget Book'),
-        ),
-      ),
+    return SafeArea(
+      child: Scaffold(
+          body: Row(
+        textDirection: TextDirection.rtl,
+        children: const [
+          FlutterLogo(),
+          Expanded(
+              child: Text(
+                  "Flutter's hot reload helps you quickly and easily experiment, build UIs, add features, and fix bug faster. Experience sub-second reload times, without losing state, on emulators, simulators, and hardware for iOS and Android.")),
+          Icon(Icons.sentiment_very_satisfied),
+        ],
+      )),
     );
   }
 }
