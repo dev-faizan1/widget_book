b6c4dc1f3f3779679cbcc84456b5227a653e728a
dev-faizan1 <ch.faizan1947@gmail.com>
2023-01-31 19:18:51 +0500

    first

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
-class MyHomePage extends StatelessWidget {
 
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child:  Scaffold(
-        body: Center(
-          child: Text('Widget Book'),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: AlignmentDirectional
+              .center, //how children should be placed in stack { center,left etc....}
+          children: [
+            //First Child will be in the bottom
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.green,
+            ),
+          ],
         ),
       ),
     );
